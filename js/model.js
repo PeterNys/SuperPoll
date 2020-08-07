dff51a30dfe1dd0d01872d4c88eeb795bd4a2093
GET Academy <terje@kolderup.net>
2020-08-07 10:33:18 +0200

    x

---
+++ js/model.js
@@ -0,0 +1,33 @@
+const model = {
+    app: {
+        currentPoll: 1,
+        loggedInUser: 'per',
+        currentPage:
+    },
+    users: [
+        { username: 'per', name: 'Per', password: '123' },
+        { username: 'pål', name: 'Pål', password: '123' },
+        { username: 'espen', name: 'Espen', password: '123', isAdmin: true },
+    ],
+    polls: [{
+        id: 1,
+        createdBy: 'espen',
+        isOpen: true,
+        question: 'Hvem er den tøffeste læreren ved GET Academy?',
+        options: ['Geir', 'Eskil', 'Terje'],
+        votes: {
+            per: 'Terje',
+            pål: 'Terje',
+            espen: 'Geir',
+        }
+    }, ]
+};
+
+/*
+    Hvilke andre måter kunne vi lagret stemmene på?
+        - Hva er best for å legge til nye stemmer?
+        - Hva er best for å endre stemmer?
+        - Hva er best for å telle opp stemmer?
+
+    Hvordan ville det blitt om hvert alternativ var et objekt, med en id?
+*/
